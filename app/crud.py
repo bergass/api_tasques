0fb679b1cbb17257dcc7f9e86612fdd06bfc6bba
bergass <albert05@gmail.com>
2025-03-10 19:13:43 +0100

    get y create tasks

--- app/crud.py
+++ app/crud.py
@@ -3,26 +3,38 @@ from models import Task
 from schemas import TaskCreate, TaskUpdate
 
 
-def get_tasks(db: Session):
-    """
-    Input:
-        db: database session
-    Output:
-        List all tasks
-    """
-    # TODO: El vostre codi va aqui
-    pass
+import sqlite3
+
+def get_tasks():
+    # Connexió a la base de dades
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+
+    # Consulta per obtenir totes les tasques
+    cursor.execute("SELECT * FROM tasks")
+    tasks = cursor.fetchall()
+
+    # Tancar la connexió
+    conn.close()
+
+    return tasks
 
 
-def create_tasks(db: Session, task: TaskCreate):
-    """
-    Input:
-        db: database session
-    Output:
-        Return the new task
-    """
-    # TODO: El vostre codi va aqui
-    pass
+
+def create_task(task_name, task_description):
+    # Connexió a la base de dades
+    conn = sqlite3.connect('tasks.db')
+    cursor = conn.cursor()
+
+    # Inserir la tasca a la taula de tasques
+    cursor.execute("INSERT INTO tasks (name, description) VALUES (?, ?)", (task_name, task_description))
+
+    # Guardar els canvis
+    conn.commit()
+
+    # Tancar la connexió
+    conn.close()
+
 
 
 def update_tasks(db: Session, task_id: int, task_update: TaskUpdate):
